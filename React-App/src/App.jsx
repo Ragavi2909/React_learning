b9f3819459bd6bba73cf0a1602689b51f45f77e6
Ragavi2909 <rrgavi03@gmail.com>
2026-05-23 19:33:21 +0530

    components concept

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -4,13 +4,18 @@ import './App.css'
 import Navbar from './Navbar'; //imported the Navbar
 import Course from './Course';
 import Footer from './Footer';
+import HTML from './assets/html.jpg'
+import CSS from './assets/css.png'
+import CSS from './assets/js.png'
 
 function App() {
   return (
     <>
-      <Navbar /> 
-      <Course />
-      <Footer />
+      
+      <Course name="HTML" price ='$199' image={html}/>
+      <Course name="CSS"  price ='$199' image={css}/>
+      <Course name ="JS" price = '$299' image={js}/>
+      
     </>
   );
 }
--- React-App/src/Course.jsx
+++ React-App/src/Course.jsx
@@ -1,19 +1,21 @@
-
+import HTML from './assets/html.jpg'
+import HTML from './assets/cs.png'
+import HTML from './assets/js.png'
 
 const course1 = "HTML";
 
-function Course(){
+function Course(props){
 
-    const styles = {
-        backgroundColor : "green",
-    }
+    
 
     return(
-        <div style={styles}>
-            <img src="" alt="" />
+        <div className="card">
+            {/* <img src={HTML} alt="" /> */}
+            <img src={props.image} alt="" />
             {/* <h3>HTML</h3> */}
-            <h3>{course1}</h3> 
-            <p>This is code io's HTML course</p>
+            <h3>{props.name}</h3> 
+            {/* <p>This is code io's HTML course</p> */}
+            <p>{props.price}</p>
         </div>
     );
 }
